9d3adc0cfcac9ff9a070ca2d9a8d288a99ade694
iPegii <51372604+iPegii@users.noreply.github.com>
2025-07-25 19:08:23 +0300

    1.2 Done

--- the_project/index.js
+++ the_project/index.js
@@ -1,29 +1,12 @@
-import fs from "node:fs";
+import express from "express"
+const app = express()
 
+const port = process.env.PORT
 
-const generateAndSaveString = () => {
-    const string = crypto.randomUUID()
-    try {
-        fs.writeFileSync('./string.txt', string);
-    } catch (err) {
-        console.error(err);
-    }
-}
-const outputString = () => {
-    try {
-        const timestamp = new Date()
-        const data = fs.readFileSync('./string.txt', 'utf8');
-        console.log(timestamp, ": ", data)
-      } catch (err) {
-        console.error(err);
-      }
-}
+app.get('/', (req, res) => {
+    res.send('hello world')
+  })
 
-const main =  () => {
-    generateAndSaveString()
-        setInterval(() => {
-            outputString()
-        },5000)
-    
-}
-main()
+app.listen(port, () => {
+console.log(`Server started in port ${port}`)
+})
