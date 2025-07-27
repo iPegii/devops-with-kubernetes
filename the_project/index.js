cd92558c7128403ee3b96b9c5d2c6585856c1687
iPegii <51372604+iPegii@users.noreply.github.com>
2025-07-27 20:10:32 +0300

    1.12 step 6 done

--- the_project/index.js
+++ the_project/index.js
@@ -1,15 +1,87 @@
-const path = require("path")
 const express = require("express")
+const fs = require("node:fs")
+const { Readable } = require("stream");
 
 const app = express()
 
-const port = process.env.PORT
+const port = 3000
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-
+app.get("/daily-image.jpg", (req, res) => {
+  const imagePath = "/usr/src/app/files/daily-image.jpg";
+  res.sendFile(imagePath);
+});
+app.get('/', async (req, res) => {
+    await checkImage()
+    res.send(`
+      <div>
+      <h1>The Project App</h1>
+      <img src="/daily-image.jpg" alt="Random image"/>
+      <p>DevOps with Kubernetes 2025</p>
+      </div>
+      `);
   })
 
 app.listen(port, () => {
 console.log(`Server started in port ${port}`)
 })
+
+const checkImage = async () => {
+    const {image,imageDate} = await readImageFromCache()
+    if(image && imageDate) {
+    const now = new Date()
+    const hour = 60 * 60 * 1000
+    if(now - imageDate <  hour) {
+      return image
+    }
+  } else {
+    updateImage()
+  }
+}
+
+const readImageFromCache = async () => {
+  try {
+    const image =  fs.readFileSync("/usr/src/app/files/daily-image.jpg");
+    const imageDate = fs.readFileSync('/usr/src/app/files/daily-image.txt', 'utf8');
+    const imageDateFormatted = new Date(imageDate)
+    return {image,imageDate:imageDateFormatted}
+  } catch (err) {
+    console.error(err);
+    return {image:null, imageDate:null}
+  }
+}
+
+const updateImage = async () => {
+  const url = "https://picsum.photos/1200";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    saveImage(await response)
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+const saveImage = async (response) => {
+  try {
+    // Write the image blob
+    console.log(response)
+    const nodeStream = Readable.fromWeb(response.body);
+    const fileStream = fs.createWriteStream("/usr/src/app/files/daily-image.jpg");
+    nodeStream.pipe(fileStream);
+
+    fileStream.on("finish", () => {
+      console.log("Image saved!");
+    });
+    try {
+      // Write the image date for caching
+      const timestamp = new Date().toISOString()
+      fs.writeFileSync('/usr/src/app/files/daily-image.txt', timestamp);
+    } catch(err) {
+      console.error(err);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
